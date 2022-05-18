73ed57201179b1fc60d4859c1b78a7ec6dca1e2f
IvaDrRad <iva.radusheva@gmail.com>
2022-05-18 09:24:39 +0300

    new task 'matching_parentheses' added

---
+++ Advanced-Python/Lists as stacks and queues/matching_parentheses.py
@@ -0,0 +1,12 @@
+expression = input()
+parentheses = []
+
+for i in range(len(expression)):
+    if expression[i] == "(":
+        parentheses.append(i)
+    elif expression[i] == ")":
+        start_index = parentheses.pop()
+        print(expression[start_index:i + 1])
+
+
+a = 1
